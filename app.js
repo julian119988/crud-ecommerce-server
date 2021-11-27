f33e965c4f5e0ba2fe48e441d900282bed502262
Julian Zambroni <julian.zambroni@gmail.com>
2021-11-27 10:05:17 -0300

    Client added to the repository

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 const apiRoutes = require("./routes/api.routes");
 const authRoutes = require("./routes/auth.routes");
 const db = require("./models/index");
+const path = require("path");
 db.sequelize.sync();
 
 var corsOptions = {
@@ -15,8 +16,13 @@ app.use(cors(corsOptions));
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "client", "build")));
+app.use(express.static("public"));
 
 app.use("/api", apiRoutes);
 app.use("/auth", authRoutes);
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 module.exports = app;
