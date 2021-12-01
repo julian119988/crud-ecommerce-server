75d336ecbe05a159ccc66971069c0a46d9d72a47
Julian Zambroni <julian.zambroni@gmail.com>
2021-12-01 19:13:48 -0300

    Changes to deploy to vercel 2

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 export const defineUriByEnviroment = () => {
     if (process.env.NODE_ENV === "production")
-        return "https://crud-ecommerce-server.vercel.app/";
+        return "https://crud-ecommerce-server.vercel.app:8080/";
     return "http://localhost:8080";
 };
